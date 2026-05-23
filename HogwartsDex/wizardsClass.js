0a511cf76b471d6d54207ba4b8c1be670b0fd49a
Rishant-Burriwar <burriwarrishant@gmail.com>
2026-05-23 19:04:21 +0530

    modified attack,defense,heal

--- HogwartsDex/wizardsClass.js
+++ HogwartsDex/wizardsClass.js
@@ -20,14 +20,72 @@ class Wizard{
         }
         this.power = randPower(30);
         this.spell = spells();
+        this.def = {
+            status:false,
+            power:0
+        }
     }
 
     attack(spellname,defender){
-        if(this.spell.find((obj)=>obj.name===spellname).type !=="attack")
+        let attackspell =this.spell.find((obj)=>obj.name===spellname)
+        if(attackspell===undefined)
+            return "INVALID INPUT"
+        if(attackspell.type !=="attack")
             return "CHOSE WRONG SPELL"
-        let attPow = this.power/5 + (randPower(this.spell[0].maxDamage));
+        let attPow = Math.round(this.power/5 + (randPower(attackspell.maxDamage)))
+        if(defender.def.status === true){
+            let defPow = defender.def.power
+            if(defPow < attPow){
+                attPow -= defPow;
+                defPow = 0 
+                defender.def.power = defPow;
+                defender.def.status =false;
+                defender.health -= attPow;
+                return `${this.name} used ${attackspell.name}\n${defender.name} Lost ${attPow} health points and Shield is Deactivated`
+            }
+            else if(defPow > attPow){
+                defPow -= attPow
+                attPow =0
+                defender.def.power = defPow;
+                return `${this.name} used ${attackspell.name}\n${defender.name} Lost ${attPow} health points Beacuse of Shield`
+            }
+            else{
+                attPow = 0;
+                defender.def.power = 0;
+                defender.def.status =false;
+                return `${this.name} used ${attackspell.name}\n${defender.name} Lost ${attPow} health points Beacuse of Shield`
+            }
+        }
         defender.health -= attPow;
-        return `${this.name} used ${this.spell[0].name}\n${defender.name} Lost ${attPow} health points`
+        return `${this.name} used ${attackspell.name}\n${defender.name} Lost ${attPow} health points`
+    }
+
+    heal(spellname){
+        let healthspell =this.spell.find((obj)=>obj.name===spellname)
+        if(healthspell===undefined)
+            return "INVALID INPUT"
+        if(healthspell.type !=="health")
+            return "CHOSE WRONG SPELL"
+        let healPow = Math.round(this.power/5 + (randPower(healthspell.maxHeal)));
+        this.health += healPow
+        if(this.health >100)
+            this.health = 100;
+        return `${this.name} used ${healthspell.name} to increase health points by ${healPow}`
+    }
+
+    defense(spellname){
+        if(this.def.status === true){
+            return "Shield already activated"
+        }
+        let defspell =this.spell.find((obj)=>obj.name===spellname)
+        if(defspell===undefined)
+            return "INVALID INPUT"
+        if(defspell.type !=="defense")
+            return "CHOSE WRONG SPELL"
+        let defPow = Math.round(this.power/5 + randPower(defspell.maxShield));
+        this.def.status = true;
+        this.def.power = defPow;
+        return "Shield Activated"
     }
 }
 
