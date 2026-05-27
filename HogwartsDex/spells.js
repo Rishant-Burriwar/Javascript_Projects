ade0e5517342da6ed38ae9d6835304d44f9c23f7
Rishant-Burriwar <burriwarrishant@gmail.com>
2026-05-27 21:48:43 +0530

    updated spell objects with min,max damage and mana

--- HogwartsDex/spells.js
+++ HogwartsDex/spells.js
@@ -2,19 +2,19 @@
 
 const validSpells = {
         attack : [
-            {name:"stupefy",type:"attack",maxDamage:20},
-            {name:"sectumsempra",type:"attack", maxDamage:35},
-            {name:"incendio",type:"attack", maxDamage:25}
+            {name:"stupefy",type:"attack",damage:{min:10,max:20},manaCost:{min:10,max:15}},
+            {name:"sectumsempra",type:"attack",damage:{min:20,max:35},manaCost:{min:25,max:40}},
+            {name:"incendio",type:"attack",damage:{min:15,max:25},manaCost:{min:15,max:25}}
         ],
         heal : [
-            {name:"episkey",type:"health", maxHeal:15},
-            {name:"vulnera sanentur",type:"health", maxHeal:35},
-            {name:"rennervate",type:"health", maxHeal:20}
+            {name:"episkey",type:"health",heal:{min:5,max:15},manaCost:{min:8,max:15}},
+            {name:"vulnera sanentur",type:"health",heal:{min:20,max:35},manaCost:{min:25,max:40}},
+            {name:"rennervate",type:"health",heal:{min:10,max:20},manaCost:{min:12,max:22}}
         ],
         defense: [
-            {name:"protego",type:"defense", maxShield:15},
-            {name:"protego paxima", type:"defense",maxShield:35},
-            {name:"fianto duri",type:"defense", maxShield:25}
+            {name:"protego",type:"defense",shield:{min:5,max:15},manaCost:{min:10,max:18}},
+            {name:"protego paxima",type:"defense",shield:{min:20,max:35},manaCost:{min:25,max:40}},
+            {name:"fianto duri",type:"defense",shield:{min:12,max:25},manaCost:{min:15,max:28}}
         ]
     }
 
