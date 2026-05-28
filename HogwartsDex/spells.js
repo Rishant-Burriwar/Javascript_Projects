565a2e1e96c2132218e7aaa94bfbcae9b3c0895f
Rishant-Burriwar <burriwarrishant@gmail.com>
2026-05-28 19:56:14 +0530

    added new spells

--- HogwartsDex/spells.js
+++ HogwartsDex/spells.js
@@ -2,20 +2,46 @@
 
 const validSpells = {
         attack : [
-            {name:"stupefy",type:"attack",damage:{min:10,max:20},manaCost:{min:10,max:15}},
-            {name:"sectumsempra",type:"attack",damage:{min:20,max:35},manaCost:{min:25,max:40}},
-            {name:"incendio",type:"attack",damage:{min:15,max:25},manaCost:{min:15,max:25}}
-        ],
-        heal : [
-            {name:"episkey",type:"health",heal:{min:5,max:15},manaCost:{min:8,max:15}},
-            {name:"vulnera sanentur",type:"health",heal:{min:20,max:35},manaCost:{min:25,max:40}},
-            {name:"rennervate",type:"health",heal:{min:10,max:20},manaCost:{min:12,max:22}}
-        ],
-        defense: [
-            {name:"protego",type:"defense",shield:{min:5,max:15},manaCost:{min:10,max:18}},
-            {name:"protego paxima",type:"defense",shield:{min:20,max:35},manaCost:{min:25,max:40}},
-            {name:"fianto duri",type:"defense",shield:{min:12,max:25},manaCost:{min:15,max:28}}
-        ]
+    {name:"stupefy",type:"attack",damage:{min:10,max:20},manaCost:{min:10,max:15}},
+    {name:"sectumsempra",type:"attack",damage:{min:20,max:35},manaCost:{min:15,max:25}},
+    {name:"incendio",type:"attack",damage:{min:15,max:25},manaCost:{min:10,max:22}},
+    {name:"confringo",type:"attack",damage:{min:18,max:30},manaCost:{min:14,max:24}},
+    {name:"bombarda",type:"attack",damage:{min:16,max:28},manaCost:{min:13,max:23}},
+    {name:"bombarda maxima",type:"attack",damage:{min:30,max:50},manaCost:{min:28,max:40}},
+    {name:"diffindo",type:"attack",damage:{min:14,max:22},manaCost:{min:10,max:18}},
+    {name:"expulso",type:"attack",damage:{min:25,max:40},manaCost:{min:22,max:34}},
+    {name:"reducto",type:"attack",damage:{min:20,max:33},manaCost:{min:18,max:30}},
+    {name:"flipendo",type:"attack",damage:{min:8,max:18},manaCost:{min:6,max:14}},
+    {name:"depulso",type:"attack",damage:{min:12,max:24},manaCost:{min:10,max:20}},
+    {name:"expelliarmus",type:"attack",damage:{min:10,max:20},manaCost:{min:8,max:16}},
+    {name:"crucio",type:"attack",damage:{min:25,max:38},manaCost:{min:24,max:36}},
+    {name:"avada kedavra",type:"attack",damage:{min:80,max:100},manaCost:{min:50,max:70}}
+],
+
+heal : [
+    {name:"episkey",type:"health",heal:{min:5,max:15},manaCost:{min:8,max:15}},
+    {name:"vulnera sanentur",type:"health",heal:{min:20,max:35},manaCost:{min:20,max:28}},
+    {name:"rennervate",type:"health",heal:{min:10,max:20},manaCost:{min:12,max:22}},
+    {name:"ferula",type:"health",heal:{min:8,max:18},manaCost:{min:10,max:18}},
+    {name:"brackium emendo",type:"health",heal:{min:15,max:25},manaCost:{min:14,max:24}},
+    {name:"essence of dittany",type:"health",heal:{min:18,max:30},manaCost:{min:16,max:26}},
+    {name:"anapneo",type:"health",heal:{min:7,max:17},manaCost:{min:9,max:16}},
+    {name:"enervate",type:"health",heal:{min:12,max:24},manaCost:{min:12,max:20}},
+    {name:"sanentur",type:"health",heal:{min:16,max:28},manaCost:{min:15,max:25}}
+],
+
+defense : [
+    {name:"protego",type:"defense",shield:{min:5,max:15},manaCost:{min:10,max:18}},
+    {name:"protego paxima",type:"defense",shield:{min:20,max:35},manaCost:{min:25,max:32}},
+    {name:"fianto duri",type:"defense",shield:{min:12,max:25},manaCost:{min:15,max:27}},
+    {name:"protego maxima",type:"defense",shield:{min:25,max:45},manaCost:{min:28,max:38}},
+    {name:"protego horribilis",type:"defense",shield:{min:22,max:40},manaCost:{min:24,max:35}},
+    {name:"protego totalum",type:"defense",shield:{min:18,max:35},manaCost:{min:20,max:30}},
+    {name:"salvio hexia",type:"defense",shield:{min:15,max:30},manaCost:{min:16,max:28}},
+    {name:"cave inimicum",type:"defense",shield:{min:14,max:28},manaCost:{min:15,max:25}},
+    {name:"repello inimicum",type:"defense",shield:{min:16,max:32},manaCost:{min:18,max:28}},
+    {name:"finite incantatem",type:"defense",shield:{min:10,max:20},manaCost:{min:10,max:20}}
+]
     }
 
 let spells = function(){
