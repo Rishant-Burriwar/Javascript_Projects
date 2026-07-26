9a665035c90d332c7bcd2b5693f5ef5945dbba96
Rishant Burriwar <burriwarrishant@gmail.com>
2026-07-26 23:36:18 +0530

    Delete HogwartsDex directory

--- HogwartsDex/api.js
+++
@@ -1,7 +0,0 @@
-async function characters(){
-    let data = await fetch("https://hp-api.onrender.com/api/characters")
-    let chardata = await data.json()
-    return chardata;
-}
-
-export {characters}
